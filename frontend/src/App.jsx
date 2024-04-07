16cf661999cc14a2f0a43c016961cd394515e6b0
Barry <barry.alpha88@outlook.fr>
2024-04-07 14:23:50 +0200

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
+
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage'
+import DashboardPage from './pages/DashboardPage';
+
+import Header from './components/Header';
+
 
 const App = () => {
   return (
-    <div className='text-4xl text-red-700'>App</div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/about' element={<AboutPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/dashboard' element={<DashboardPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
